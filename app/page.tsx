0dd2599f0b63d0072eba5ce4916877e2eafdfa43
ATREE Bangalore <70751764+atreebangalore@users.noreply.github.com>
2025-04-07 23:18:56 +0530

    trying again

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <main>
-    This is the app checking whether is working 
+    This is the app checking whether is working 2
   </main>
   );
 }
